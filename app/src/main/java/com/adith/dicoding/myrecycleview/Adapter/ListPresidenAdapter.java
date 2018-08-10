b440b7c00f6f030fe26f4dc811225200447e5a71
dhiandraaa <d.adithya.a@gmail.com>
2018-08-10 12:51:39 +0700

    Trying to understand

---
+++ app/src/main/java/com/adith/dicoding/myrecycleview/Adapter/ListPresidenAdapter.java
@@ -0,0 +1,68 @@
+package com.adith.dicoding.myrecycleview.Adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.adith.dicoding.myrecycleview.Presiden;
+import com.adith.dicoding.myrecycleview.R;
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class ListPresidenAdapter extends RecyclerView.Adapter<ListPresidenAdapter.CategoryViewHolder>{
+    private Context konteks;
+
+    public ArrayList<Presiden> getListPresiden(){
+        return listPresiden;
+    }
+    public void setListPresident(ArrayList<Presiden> listPresiden) {
+        this.listPresiden = listPresiden;
+    }
+
+    private ArrayList<Presiden>listPresiden;
+
+    public ListPresidenAdapter(Context konteks){
+        this.konteks = konteks;
+    }
+
+    @Override
+    public CategoryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemRow = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_baris_presiden, parent, false);
+        return new CategoryViewHolder(itemRow);
+    }
+
+    @Override
+    public void onBindViewHolder(CategoryViewHolder holder, int position) {
+        holder.namaPresiden.setText(getListPresiden().get(position).getName());
+        holder.remarksPresiden.setText(getListPresiden().get(position).getRemarks());
+
+        Glide.with(konteks)
+                .load(getListPresiden().get(position).getPhoto())
+                .override(55,55)
+                .crossFade()
+                .into(holder.imgPhoto);
+    }
+
+    @Override
+    public int getItemCount() {
+        return getListPresiden().size();
+    }
+
+    class CategoryViewHolder extends RecyclerView.ViewHolder{
+        TextView namaPresiden;
+        TextView remarksPresiden;
+        ImageView imgPhoto;
+
+        public CategoryViewHolder (View itemView){
+            super(itemView);
+            namaPresiden = (TextView)itemView.findViewById(R.id.nama_presiden);
+            remarksPresiden = (TextView)itemView.findViewById(R.id.remaks_presiden);
+            imgPhoto = (ImageView)itemView.findViewById(R.id.item_photo);
+        }
+    }
+}
